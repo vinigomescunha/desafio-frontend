894d014142eb945f6d7363153a1ec8e44063f84e
Vinicius Gomes <vinigomescunha@gmail.com>
2020-02-03 12:08:17 -0300

    soy loco por ti america

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   entry: './index.js',
   output: {
     path: path.join(__dirname, 'dist'),
-    publicPath: '/dist/',
+    publicPath: '/',
     filename: 'index.js'
   },
   plugins: [
